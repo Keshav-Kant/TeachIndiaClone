05e98619fa7eb7ca3f1bae1178677e7d6cced45e
Keshav-Kant <keshavkant7859@gmail.com>
2025-01-25 10:18:48 +0530

    First Commit

---
+++ login.js
@@ -0,0 +1,32 @@
+// there is a man who is 20 years older than his son who was 5 year back in 2019 what is current age of father..a
+
+// let x - bucket be the son age and
+// let y be the fathers age 
+
+
+
+// variables = x,y, a-z A-z or numberOfChild
+// constant = g=9.8 m/s ,10
+
+
+// var numberOfChild = 2;
+// console.log("This is a variable",numberOfChild);
+
+// const numberOfChild = 4;
+// console.log("This is a constant",numberOfChild);
+
+// numberOfChild = 5
+// console.log("This is the changed Constant",numberOfChild);
+
+// + * / - = : operators
+
+// logical : &&=and ||=or !=not
+// relational > < == ===
+
+
+//0 means (false) 1 means (true)
+
+
+// console.log(10 === '10')
+
+console.log(Date())
